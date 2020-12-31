77ebb41c6ceb1e331d75b758ac1f2e52ab1bd892
Dwi A Lestari <dwitari1809@gmail.com>
2020-12-31 14:20:37 +0700

    Add files via upload

---
+++ howto.php
@@ -0,0 +1,93 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+        <meta name="description" content="PHP Framework Installer for Free Hosting. You can easy use it!">
+    <meta name="keywords" content="PHP Framework Installer for Free Hosting">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="author" content="Dwi Ayu Lestari (@dtari18)">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <title>Project Installer Framework</title>
+    <style type="text/css">
+      .footerteks1{
+        color:white;
+      }
+    </style>
+  </head>
+  <body>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-info">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+    <a href="index.php" class="navbar-brand">PFI</a>
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li class="nav-item">
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="howto.php">How to?</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="editor.php">Teks Editor</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+<br />
+<center><h3>Panduan Singkat dalam Penggunaan Situs Ini :</h3></center><br/>
+<div class="container">
+  <h4>1. Upload Project Website yang Ada.</h4>
+  <p>a) Pilihlah form "Upload Project" untuk memasukkan project website milik kamu yang ada atau telah dibuat.<br>
+  b) Pindahkan project web yang telah diupload ke folder html_public, home/www, atau direktori home yang terdapat di hosting gratis melalui pengaturan File Manager di Control Panel pada hosting masing-masing.</p>
+  <h4>2. Membuat Project Website Baru dengan PHP Framework</h4>
+  <p>a) Pilihlah form "Install PHP Framework" untuk membuat project website baru dengan sesuai memilih PHP Framework dengan sesuai keinginan atau kebutuhan.<br>
+  b) Pindahkan project web yang telah diupload ke folder html_public, home/www, atau direktori home yang terdapat di hosting gratis melalui pengaturan File Manager di Control Panel pada hosting masing-masing.</p>
+  <br/>
+  <p>Kamu tidak perlu melakukan extract project website lagi dalam upload project web atau pemasangan PHP Framework secara manual lagi. Kamu tinggal ngoding project web sambil ngopi saja.</p> 
+</div>
+<hr />
+<center><h3>Panduan Pemasangan Aplikasi Ini ke dalam Hosting Gratis :</h3></center><br/>
+<div class="container">
+<p>1. Masuk ke Control Panel pada hosting gratis kamu</p>
+<p>2. Klik File Manager di Control Panel.</p>
+<p>3. Klik Folder "public_html" atau "www".</p>
+<p>4. Klik Upload di dalam folder tersebut.</p>
+<p>5. Pilihlah file "phpdtfrains.zip", klik Open atau OK</p>
+<p>6. Extract file .zip tersebut.</p>
+<p>7. Buka situs http://namadomain.com/phpdtfrains/index.php </p>
+<p>Ada perbedaan step by step yang sedikit pada masing-masing hosting gratis.</p>
+</div>
+<hr />
+<center><h3>Perhatian Sebelum Upload Project :</h3></center><br/>
+<div class="container">
+  <p align="justify">Masing-masing pihak penyedia hosting gratis memberikan batasan upload file maksimal 2 MB sampai dengan 10 MB. Apabila ingin upload project website dengan berukuran file lebih dari batasan (limit), kamu bisa mengedit pada php.ini sendiri atau minta bantuan kepada penyedia hosting karena pihak pengembang ini belum menyediakan file custom php.ini sendiri.</p>
+  <p>Aplikasi ini juga berlaku untuk hosting berbayar, disini kami memfokuskan untuk pengguna hosting gratis dalam memudahkan untuk menciptakan project website yang berguna bagi diri-sendiri dan orang lain.</p>
+  <p>Perhatikan versi PHP yang digunakan pada hosting gratis sebelum melakukan pembuatan project web baru maupun upload project web yang ada, mencegah kesalahan aplikasi berbasis web yang disebabkan versi PHP yang tidak mendukung. Kamu bisa mengubah versi PHP melalui Control Panel yang disediakan hosting gratis.</p>
+</div>
+<hr />
+<center><h3>Kontak Developer :</h3></center><br/>
+<div class="container">
+  <p><i class="fas fa-id-card"></i> Dwi Ayu Lestari</p>
+  <p><i class="fab fa-whatsapp"></i> 085207364117</p>
+  <p><i class="fab fa-facebook-square"></i> fb.com/dtari18</p>
+  <p><i class="fab fa-telegram"></i> dtari18 / 085207364117</p>
+  <p></p>
+</div>
+<footer id="sticky-footer" class="py-4 bg-info text-white-50">
+    <div class="container text-center">
+      <small class="footerteks1">Copyright 2020 &copy; PHP Framework Installer Versi 0.1, Developed By Dwi Ayu Lestari</small>
+    </div>
+</footer>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+  </body>
+</html>
